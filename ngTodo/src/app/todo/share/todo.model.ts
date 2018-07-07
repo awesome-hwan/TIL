41a12b20425abdfb9f45b5f4dbe94c2cc6435bdf
Jr.byeon <bun920528@gmail.com>
2018-07-07 17:50:50 +0900

    ng css

--- ngTodo/src/app/todo/share/todo.model.ts
+++ ngTodo/src/app/todo/share/todo.model.ts
@@ -1,4 +1,4 @@
 export class Todo {
-  done: boolean,
+  done: boolean;
   text: string;
 }
--- ngTodo/src/app/todo/todos/add-todo/add-todo.component.ts
+++ ngTodo/src/app/todo/todos/add-todo/add-todo.component.ts
@@ -3,13 +3,40 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'app-add-todo',
   template: `
-    <div>
-      <input type="text" placeholder="할 일 추가" [(ngModel)]="newText">
-    </div>
-    <button (click)="addTodo(newText)">Add</button>
+    <button (click)="addTodo(newText)">+</button>
+    <input type="text" placeholder="할 일 추가" [(ngModel)]="newText">
+    
 
   `,
-  styles: []
+  styles: [`
+    :host {
+      display: block;
+      padding: 16px 16px 16px 10px;
+      background-color: white;
+    }
+    input {
+      display:inline-block;
+      font-size: 18px;
+      border: none;
+      
+    }
+    
+    input:focus, button:focus {
+      outline: none;
+    }
+    
+    button {
+      width: 24px;
+      height: 24px;
+      border-radius: 24px;
+      color: white;
+      font-size: 16px;
+      line-height: 17px;
+      border: 1px solid dimgray;
+      background-color: darkblue;
+      
+    }
+  `]
 })
 export class AddTodoComponent implements OnInit {
 
